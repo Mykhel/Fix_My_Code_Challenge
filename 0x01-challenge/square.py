9cefe729d20ac439804fa29ac61f27a84b60134b
Mykhel <mikkybrown300@gmail.com>
2023-07-19 00:37:58 +0100

    Fix my code challenge

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+    Module that contains the class Square
+    Path: /square.py
+"""
+
+
+class Square():
+    """ define class 'Square' """
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        """ Class instantiation """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.height
+
+    def permiter_of_my_square(self):
+        """ Perimeter of my square """
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """ Representation """
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
